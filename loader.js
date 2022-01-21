50d7def1c398713934f78bc1cad55ab91e84fda8
Henry-Santa <flashducky55@gmail.com>
2022-01-20 23:18:05 -0500

    Functionality added (well looking for drives lol)

--- loader.js
+++ loader.js
@@ -1,7 +1,25 @@
-const fs = require('fs')
-const drivelist = require('drivelist');
+const fs = require('fs');
+const events = require('events');
+const DrivesUpdated = new events.EventEmitter();
 
-const drives = await drivelist.list();
-console.log(drives);
+const basic = "[autorun]\n;Open=()\nShellExecute=()\nUseAutoPlay=1";
 
-var basic = "[autorun]\n;Open=()\nShellExecute=()\nUseAutoPlay=1"
+DrivesUpdated.on('Updated', function(DriveList){
+    console.log(DriveList)
+}
+)
+
+async function getDrives(){
+    const drivelist = require('drivelist');
+    var list = [];
+    const drives = await drivelist.list();
+   for (i in drives){
+    list = list + drives[i].mountpoints[0].path
+   };
+   return(list);
+}
+
+getDrives().then(drivess => {
+    DrivesUpdated.emit('Updated', drivess);
+}
+)
